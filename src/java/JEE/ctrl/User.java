16874ccadd48d7d83c71b3e86c1f2e9131b1cdfa
jourdain <hjl_max@msn.com>
2017-11-16 15:27:56 +0100

    test

---
+++ src/java/JEE/ctrl/User.java
@@ -0,0 +1,32 @@
+package JEE.ctrl;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Hugo
+ */
+public class User {
+    private String login;
+    private String pw;
+    
+    public void setLogin(String login){
+        this.login = login;
+    }
+    
+    public String getLogin(){
+        return this.login;
+    }
+    
+    public void setPw(String pw){
+        this.pw = pw;
+    }
+    
+    public String getPw(){
+        return this.pw;
+    }
+}
---
+++ web/home-connected.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : home-connected
+    Created on : Nov 16, 2017, 3:16:59 PM
+    Author     : Hugo
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
